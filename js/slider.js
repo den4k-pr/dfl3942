6afcfcb3292c6528bbb857edfdfaaffcb466c9c5
Denys <yakovden4k@gmail.com>
2026-06-19 15:27:02 +0300

    first commit

---
+++ js/slider.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const galleryEl = document.querySelector(".gallery-swiper");
+    if (!galleryEl) return;
+
+    new Swiper(galleryEl, {
+        loop: true,
+        spaceBetween: 20,
+        slidesPerView: 2,
+        centeredSlides: false,
+        // Кнопки навігації видалено згідно з ТЗ
+        
+        pagination: {
+            el: ".gallery-slider-pagination",
+            clickable: true,
+            bulletClass: 'swiper-pagination-bullet',
+            bulletActiveClass: 'swiper-pagination-bullet-active',
+        },
+
+        navigation: {
+            nextEl: ".s8__arrow-next",
+            prevEl: ".s8__arrow-prev"
+        }
+    });
+    
+    
+});
+document.addEventListener("DOMContentLoaded", () => {
+    const sliderEl = document.querySelector(".second-swiper");
+    if (!sliderEl) return;
+
+    new Swiper(sliderEl, {
+        loop: false,
+        spaceBetween: 14,
+        slidesPerView: 'auto',
+
+        pagination: {
+            el: ".second-slider-pagination",
+            clickable: true,
+            bulletClass: 'swiper-pagination-bullet',
+            bulletActiveClass: 'swiper-pagination-bullet-active',
+        },
+
+        navigation: {
+            nextEl: ".s7__arrow-next",
+            prevEl: ".s7__arrow-prev"
+        }
+    });
+});
